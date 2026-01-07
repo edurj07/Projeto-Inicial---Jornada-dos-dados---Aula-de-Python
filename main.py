06f9d89794562c148ffbc1d61ba4b2fd2c8adf97
Eduardo <edurj07@gmail.com>
2026-01-07 19:04:52 -0300

    Meu primeiro commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Hello World")
+
+#VARIÁVEIS
+idade = 47
+nome = "Eduardo GG"
+
+#print
+print(idade)
+print(nome)
+
+#nova idade em breve
+New_Year = idade + 1
+print(New_Year)
+
+# Lista Valores
+lista = [1,2,3,4,5]
+
+#Métodos
+lista_invertida = lista.reverse
